48558a89bc866f093b9d2a891ab56badc9207e66
Stefan Bussemaker <56066664+kad-busses@users.noreply.github.com>
2021-10-29 10:26:26 +0200

    fix: add options to disable web interface (#12)
    Closes https://github.com/kadaster-labs/sensrnet-central-viewer-geoserver/issues/11

--- build_src/scripts/entrypoint.sh
+++ build_src/scripts/entrypoint.sh
@@ -14,6 +14,9 @@ export GEOSERVER_OPTS="-Djava.awt.headless=true -server -Xms${INITIAL_MEMORY} -X
 
     #    -Dlog4j.configuration=file:///${GEOSERVER_DATA_DIR}/log4j.xml \
 
+CONSOLE_DISABLED="${geoserver_console_disabled:-false}"
+export GEOSERVER_OPTS="${GEOSERVER_OPTS} -DGEOSERVER_CONSOLE_DISABLED=${CONSOLE_DISABLED}"
+
 ## Preparare the JVM command line arguments
 export JAVA_OPTS="${JAVA_OPTS} ${GEOSERVER_OPTS}"
 
